e0daf129ee92c5a211a3b3dfd3a1556651fa5ba4
timofeymac <timsidorin@gmail.com>
2025-03-10 15:35:09 +1000

    fix migrations

--- backend/routing/events_router.py
+++ backend/routing/events_router.py
@@ -1,12 +1,12 @@
 
 from fastapi import APIRouter, Depends, HTTPException
 
-from schemas.events import Event_create
+from schemas.events import EventCreate
 
 router = APIRouter(prefix="/event", tags=["Мероприятия"])
 
 
 
 @router.post("/create")
-async def create_event(user_data: Event_create):
+async def create_event(user_data: EventCreate):
     pass
