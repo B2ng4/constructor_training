124c07644fb2f3967d10505e22cd88a77f3d9ce3
timsidorin <timsidorin@gmail.com>
2025-03-29 18:27:42 +0700

    Изменил модели тренингов

--- backend/schemas/trainings.py
+++ backend/schemas/trainings.py
@@ -1,37 +1,109 @@
 from datetime import datetime
-from typing import Optional, Dict, Any
-from pydantic import BaseModel
+from typing import List, Optional, Dict, Any
+from pydantic import BaseModel, UUID4
 from uuid import UUID
 
-class TrainingCreate(BaseModel):
+
+
+
+
+
+class TrainingBase(BaseModel):
+    """Базовый тренинг"""
     title: str
-    type_id: int
+    description: str
     start_date: Optional[datetime] = None
     end_date: Optional[datetime] = None
-    image_uuid: Optional[UUID] = None
+    cover_image_uuid: Optional[UUID4] = None
+
+
+
+class TrainingStepBase(BaseModel):
+    """Шаг Тренинга"""
+    step_number: int
+    image_url: str
+    area: Dict[str, int]
+    description: str
+
+
+class TrainingStepCreate(TrainingStepBase):
+    pass
+
+
+
+class TrainingCreate(TrainingBase):
+    """Создание тренига (добавление шагов и типа тренинга)"""
+    steps: List[TrainingStepCreate]
+    type_id: Optional[int] = None
 
 
 class TrainingUpdate(BaseModel):
+    """Обновление тренига (Обновление  шагов и типа тренинга)"""
     title: Optional[str] = None
-    type_id: Optional[int] = None
+    description: Optional[str] = None
     start_date: Optional[datetime] = None
     end_date: Optional[datetime] = None
-    image_uuid: Optional[UUID] = None
+    cover_image_uuid: Optional[UUID4] = None
+    type_id: Optional[int] = None
 
 
-class TrainingResponse(BaseModel):
+
+
+class TrainingStepResponse(TrainingStepBase):
     id: int
-    title: str
-    type_id: int
-    start_date: datetime
-    end_date: Optional[datetime] = None
-    image_uuid: Optional[UUID] = None
+    training_id: int
+
+    class Config:
+        from_attributes = True
+
+
+
+# Модели для назначения тренингов
+class TrainingAssignmentCreate(BaseModel):
+    user_ids: List[int]
+
+class TrainingAssignmentUpdate(BaseModel):
+    completed: bool
+
+
+
+class TrainingResponse(TrainingBase):
+    id: int
+    creator_id: int
+    created_at: datetime
+    type_id: Optional[int] = None
+    steps: List[TrainingStepResponse]
+
+    class Config:
+        from_attributes = True
+
+
+
+class TrainingAssignmentResponse(BaseModel):
+    id: int
+    user_id: int
+    assigned_at: datetime
+    completed_at: Optional[datetime] = None
 
     class Config:
         from_attributes = True
 
 
 class TrainingDetailResponse(TrainingResponse):
-    type_name: str
+    assignments: List[TrainingAssignmentResponse]
+    type_name: Optional[str] = None
     type_data: Optional[Dict[str, Any]] = None
-    image_uuid: Optional[UUID] = None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
