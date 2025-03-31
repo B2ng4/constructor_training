61d3a416d07a168045b3c506b42e2c67c226ebcd
TimofeyProjects <101704774+tim26006@users.noreply.github.com>
2025-03-31 16:17:57 +1000

    Переделка конфигурации

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ from routing import auth_router, training_router
 from core.config import configs
 from core.create_base_app import create_base_app
 
+
 app = create_base_app(configs)
 app.include_router(auth_router.router)
 app.include_router(training_router.router)
@@ -22,7 +23,7 @@ app.include_router(training_router.router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="localhost", port=8002)
+    uvicorn.run("main:app", host=configs.HOST, port=configs.PORT)
 
 
 # uvicorn.run("main:app", host="localhost", port=8000, reload=True)
