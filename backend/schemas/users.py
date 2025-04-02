e6e00e96a6c2348950cf59e51cec204c61a73b07
TimofeyProjects <101704774+tim26006@users.noreply.github.com>
2025-04-02 16:35:57 +1000

    Лопачу модели 1.1

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -11,12 +11,22 @@ import re
 
 class User(BaseModel):
     email: EmailStr = Field(..., description="Электронная почта")
+    phone_number: str = Field(
+        ..., description="Номер телефона в международном формате, начинающийся с '+'"
+    )
     first_name: str = Field(
         ..., min_length=3, max_length=50, description="Имя, от 3 до 50 символов"
     )
     last_name: str = Field(
         ..., min_length=3, max_length=50, description="Фамилия, от 3 до 50 символов"
     )
+
+    @field_validator("phone_number")
+    @classmethod
+    def validate_phone_number(cls, value: str) -> str:
+        if not re.match(r"\d{5,15}", value):
+            raise ValueError("Номер телефона должен  содержать от 5 до 15 цифр")
+        return value
 
     class Config:
         from_attributes = True
