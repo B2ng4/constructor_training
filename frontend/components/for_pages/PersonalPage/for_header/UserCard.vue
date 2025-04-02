254e9fb7e0d01a9a2f3746b97b57a1db1ba1350f
Konstantin Chernenko <kostyan2004tt@mail.ru>
2025-04-02 16:51:04 +1000

    PersonalPage
    Начало работы над личным кабинетом

---
+++ frontend/components/for_pages/PersonalPage/for_header/UserCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <q-btn rounded color="white text-black" style="width: auto;">
+        <q-icon class="q-mr-xs" name="person" />
+        Имя юзера
+        <q-menu fit>
+            <q-list style="min-width: 100px">
+                <q-item clickable>
+                <q-item-section>Перейти в профиль</q-item-section>
+                </q-item>
+            </q-list>
+        </q-menu>
+    </q-btn>
+</template>
+
+<script>
+export default {
+    name: 'UserCard'
+}
+</script>
+
+<style scoped>
+
+</style>
