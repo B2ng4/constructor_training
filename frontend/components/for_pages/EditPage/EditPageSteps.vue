a102afdb8c646a2345515cffd502e88debc354ad
B2ng4 <kostyan2004tt@mail.ru>
2025-05-16 20:30:04 +1000

    Изменение порядка шагов
    Кто-то сделал нерабочую апиху и даже не проверил ее

--- frontend/components/for_pages/EditPage/EditPageSteps.vue
+++ frontend/components/for_pages/EditPage/EditPageSteps.vue
@@ -8,8 +8,10 @@
 		>
 			<template #item="{element}">
 				<BaseCard
+					@click="selectedTrainingStep(element)"
 					class="q-mb-md cursor-move"
 					:key="element.step_number"
+					:class="{ 'selected-steps': selectTrainingStep === element }"
 				>
 					<template v-slot:title>
 						Шаг: {{ element.step_number }}
@@ -18,12 +20,13 @@
 			</template>
 		</draggable>
 	</div>
-	<q-btn class="full-width" flat color="secondary" label="Добавить шаг" />
+	<q-btn class="full-width" @click="updateSteps" flat color="secondary" label="Добавить шаг" />
 </template>
 
 <script>
 import BaseCard from "@components/BaseComponents/BaseCard.vue";
 import draggable from 'vuedraggable';
+import axios from "axios";
 export default {
 	name: "EditPageSteps",
 	components: { BaseCard, draggable},
@@ -36,14 +39,39 @@ export default {
 		return {
 			drag: false,
 			mySteps: [],
+			selectTrainingStep: null
+		}
+	},
+	methods: {
+		selectedTrainingStep(stepNumber) {
+			this.selectTrainingStep = stepNumber;
+		},
+		async updateSteps() {
+			let payload = this.mySteps;
+			let response = await axios.put(`${__BASE__URL__}/training/${this.$route.params.uuid}`, {payload});
+			try {
+				console.log(response);
+			} catch (error) {
+				console.log(error);
+			}
 		}
 	},
 	watch: {
-		//Хорошая практика, когда мы не изменяем пропсы () => поэтому когда данные приходят, заносим в локальную переменную
 		steps(newVal, oldVal) {
 			if (newVal !== oldVal) {
 				this.mySteps = newVal;
+				this.selectTrainingStep = this.mySteps[0];
 			}
+		},
+		mySteps: {
+			handler(newVal) {
+				if (newVal) {
+					this.mySteps.forEach((step, index) => {
+						step.step_number = index + 1;
+					});
+				}
+			},
+			deep: true
 		}
 	}
 };
@@ -52,5 +80,11 @@ export default {
 <style scoped>
 .my-card :last-child {
 	display: none;
+}
+
+.selected-steps {
+	border: 2px solid #6274F8;
+	transition: all 0.1s ease-out;
+	transform: scale(1.03);
 }
 </style>
