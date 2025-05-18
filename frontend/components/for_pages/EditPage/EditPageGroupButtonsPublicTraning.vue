7a6991597d7df6c46b0ec7ef192f78a87217509b
B2ng4 <kostyan2004tt@mail.ru>
2025-05-18 22:41:41 +1000

    FULL TRASH

---
+++ frontend/components/for_pages/EditPage/EditPageGroupButtonsPublicTraning.vue
@@ -0,0 +1,15 @@
+<template>
+	<q-btn class="toolbar-btn">
+			<span class="btn-label">Опубликовать</span>
+	</q-btn>
+</template>
+
+<script>
+export default {
+	name: "EditPageGroupButtonsPublicTraning"
+};
+</script>
+
+<style scoped>
+
+</style>
--- frontend/pages/EditPage.vue
+++ frontend/pages/EditPage.vue
@@ -1,17 +1,18 @@
 <template>
-	<q-layout view="hHh lpR fFf">
+	<q-layout :class="{'display-none': statusEdit}"  view="hHh lpR fFf">
 		<q-drawer width="250" show-if-above side="left" bordered>
 			<EditPageSteps @select-url="selectTrainingUrl" :steps="traininData.steps"/>
-		</q-drawer>
+			</q-drawer>
 
-		<q-page-container>
-			<div class="page-container" :class="{'justify-center': statusSteps}">
-				<EditPageToolbar v-if="!statusSteps" style="margin-left: 5%"/>
+		<q-page-container  style="overflow: hidden">
+			<EditPageGroupButtons @edit-training="editTrainingStatus"/>
+			<div class="page-container justify-center">
 				<EditPageUploadPhoto v-if="statusSteps"/>
 				<EditPagePhotoView v-else :steps="steps"/>
 			</div>
 		</q-page-container>
 	</q-layout>
+	<EditPageEditPhoto :steps="steps" :status-edit="statusEdit"/>
 </template>
 
 <script>
@@ -20,13 +21,15 @@ import axios from "axios";
 import EditPageUploadPhoto from "@components/for_pages/EditPage/EditPageUploadPhoto.vue";
 import EditPageSteps from "@components/for_pages/EditPage/EditPageSteps.vue";
 import EditPagePhotoView from "@components/for_pages/EditPage/EditPagePhotoView.vue";
-import EditPageToolbar from "@components/for_pages/EditPage/EditPageToolbar.vue";
+import EditPageGroupButtons from "@components/for_pages/EditPage/EditPageGroupButtons.vue";
+import EditPageEditPhoto from "@components/for_pages/EditPage/EditPageEditPhoto.vue";
 export default {
-	components: {EditPageUploadPhoto, EditPageSteps, EditPagePhotoView, EditPageToolbar},
+	components: {EditPageUploadPhoto, EditPageSteps, EditPagePhotoView, EditPageGroupButtons, EditPageEditPhoto},
 	data() {
 		return {
 			traininData: {},
 			steps: '',
+			statusEdit: false,
 		}
 	},
 	computed: {
@@ -43,6 +46,9 @@ export default {
 		},
 		selectTrainingUrl(url) {
 			this.steps = url;
+		},
+		editTrainingStatus(status) {
+			this.statusEdit = status;
 		}
 	},
 	mounted() {
@@ -56,7 +62,6 @@ export default {
 	position: relative;
 	display: flex;
 	align-items: center;
-	min-height: 100vh;
 	grid-gap: 5%
 }
 </style>
