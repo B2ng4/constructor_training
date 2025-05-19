b7467ec10bcd5458a77d2dcccf9e322d5840b8ec
B2ng4 <kostyan2004tt@mail.ru>
2025-05-19 23:17:02 +1000

    transfer vue -> react

--- frontend/pages/EditPage.jsx
+++ frontend/pages/EditPage.jsx
@@ -1,13 +1,34 @@
 import { useParams } from "react-router";
 import { Excalidraw } from "@excalidraw/excalidraw";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+//Получаем тренинг
+async function getTrainings(uuid) {
+	let result = await axios.get(`${__BASE__URL__}/training/` + uuid);
+	try {
+		return result.data;
+	} catch (error) {
+		return error;
+	}
+}
 
 function EditPage() {
 	const { uuid } = useParams();
+	const [dataTraining, setDataTraining] = useState(null);
 
+	useEffect(() => {
+		async function fetchData() {
+			const data = await getTrainings(uuid);
+			setDataTraining(data);
+		}
+		fetchData();
+	}, [uuid]);
 	return (
 		<>
+			<h1>{dataTraining?.title}</h1>
 		<div style={{ height: "900px" }}>
-			<Excalidraw />
+			<Excalidraw/>
 		</div>
 		</>
 	);
--- frontend/react-app/main.jsx
+++ frontend/react-app/main.jsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import EditPage from '@pages/EditPage';
 import { BrowserRouter, Routes, Route } from "react-router";
 import "@excalidraw/excalidraw/index.css";
 
 createRoot(document.getElementById('react-app')).render(
-	<StrictMode>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/training/edit/:uuid" element={<EditPage />} />
 			</Routes>
 		</BrowserRouter>
-	</StrictMode>
 );
