9312b8ef0e03d1e7dccda1daaaba9ddc06730b5a
B2ng4 <kostyan2004tt@mail.ru>
2025-05-20 23:37:22 +1000

    Broken library
    Придется самому верстать редактор ((((

--- frontend/components/for_pages/EditPage/EditPageBaseCanvas.jsx
+++ frontend/components/for_pages/EditPage/EditPageBaseCanvas.jsx
@@ -1,13 +1,33 @@
 import styles from '@assets/styles/for-components/base-canvas.module.css';
+import menuIcon from '@assets/img/menu.svg';
+import React from "react";
 
-function BaseCanvas(props) {
+function DropDown() {
+	return (
+		<div className={styles['drop-down']}/>
+	);
+}
+
+function ButtonsGroup() {
+	const [statusDropdown, setStatusDropdown] = React.useState(false);
 	return (
 		<div className="fit">
 			<div className={styles['left-button']}>
-				<button>Click me</button>
-				<button>Click me</button>
+				<button onClick={() => setStatusDropdown(!statusDropdown)}>
+					<img src={menuIcon} alt="menu" />
+				</button>
+				<button>
+					Страница 1
+				</button>
 			</div>
+			{statusDropdown && <DropDown />}
 		</div>
+	);
+}
+
+function BaseCanvas(props) {
+	return (
+		<ButtonsGroup/>
 	);
 }
 
