0a57ff793434ad10121d2cd4dd0324ef24812ed5
B2ng4 <125038202+B2ng4@users.noreply.github.com>
2025-10-20 12:09:21 +1000

    refactor: Снес react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar } from '@quasar/vite-plugin';
 import path from 'path';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
     vue(),
     quasar(),
-		react()
   ],
   define: {
     __BASE__URL__: JSON.stringify('http://localhost:8002'),
